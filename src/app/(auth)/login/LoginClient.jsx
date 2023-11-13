cc0f57f54cf318e1f938dd2c4c406c3ae7f73e11
lbo728 <72309817+lbo728@users.noreply.github.com>
2023-11-13 22:54:09 +0900

    feat: 로그인 UI

---
+++ src/app/(auth)/login/LoginClient.jsx
@@ -0,0 +1,58 @@
+'use client'
+import Image from 'next/image';
+import React, { useState } from 'react'
+
+import LogoPath from '@/assets/colorful.svg'
+import { useRouter } from 'next/navigation';
+
+import styles from "./Auth.module.scss";
+
+const LoginClient = () => {
+  const [email, setEmail] = useState(''); 
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAutoLogin, setIsAutoLogin] = useState(false);
+
+  const router = useRouter();
+
+  const redirectUser = () => {
+    router.push('/');
+  }
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+  }
+
+  const signInWithGoogle = () => {
+
+  }
+
+  return (
+    <section className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
+          <Image priority src={LogoPath} alt="logo" />
+        </h1>
+        <form onSubmit={loginUser} className={styles.form}>
+          {/* Input */}
+          Input
+        </form>
+        <div className={styles.group}>
+          {/* 자동 로그인, 비밀번호 수정 */}
+          자동 로그인, 비밀번호 수정
+        </div>
+        <div className={styles.buttonGroup}>
+          {/* Button */}
+          Button
+          <div>
+            {/* Button */}
+            Button
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default LoginClient
